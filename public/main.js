ff0ffb086eacf5925e59f266f8af328152851273
rafajr91 <rah5024@gmail.com>
2017-10-12 16:46:09 -0700

    Created functions to render thumbnail images of game data sample. #1

--- public/main.js
+++ public/main.js
@@ -7,16 +7,75 @@ function getGames() {
     })
     .then(games => {
       games
-        .map(renderGame)
-        .forEach($game => {
-          $gamesList.appendChild($game)
+        .map(renderGameThumb)
+        .forEach($gameThumb => {
+          $gamesList.appendChild($gameThumb)
         })
     })
 }
 
 getGames()
 
-function renderGame(game) {
+function renderGameThumb(game) {
+  const $gameThumb = document.createElement('div')
+  const $img = document.createElement('img')
+  const $caption = document.createElement('div')
+  const $captionHeader = document.createElement('h3')
+  const $detailContainer = document.createElement('div')
+  const $detailList = document.createElement('ul')
+  const $detailOne = document.createElement('li')
+  const $detailTwo = document.createElement('li')
+  const $detailThree = document.createElement('li')
+  const $buttonContainer = document.createElement('div')
+  const $rateButton = document.createElement('a')
+  const $detailsButton = document.createElement('a')
+
+  const { name, genre, rating, esrb, imgSrc } = game
+
+  $gameThumb.setAttribute('class', 'col-md-2 thumbnail')
+
+  $img.setAttribute('class', 'game-icon')
+  $img.setAttribute('src', imgSrc)
+  $img.setAttribute('alt', name)
+
+  $caption.setAttribute('class', 'caption')
+
+  $captionHeader.textContent = name
+
+  $detailOne.textContent = 'Genre: ' + genre
+  $detailTwo.textContent = 'Rating: ' + rating
+  $detailThree.textContent = 'ESRB: ' + esrb
+
+  $detailList.appendChild($detailOne)
+  $detailList.appendChild($detailTwo)
+  $detailList.appendChild($detailThree)
+
+  $detailContainer.appendChild($detailList)
+
+  $rateButton.setAttribute('href', '#')
+  $rateButton.setAttribute('class', 'btn btn-primary')
+  $rateButton.setAttribute('role', 'button')
+  $rateButton.textContent = 'Rate'
+
+  $detailsButton.setAttribute('href', '#')
+  $detailsButton.setAttribute('class', 'btn btn-default')
+  $detailsButton.setAttribute('role', 'button')
+  $detailsButton.textContent = 'More Details'
+
+  $buttonContainer.appendChild($rateButton)
+  $buttonContainer.appendChild($detailsButton)
+
+  $caption.appendChild($captionHeader)
+  $caption.appendChild($detailContainer)
+  $caption.appendChild($buttonContainer)
+
+  $gameThumb.appendChild($img)
+  $gameThumb.appendChild($caption)
+
+  return $gameThumb
+}
+
+/* function renderGame(game) {
   const $game = document.createElement('div')
   const $gameTitle = document.createElement('h3')
   const $img = document.createElement('img')
@@ -30,6 +89,7 @@ function renderGame(game) {
   const $detailSeven = document.createElement('li')
   const { genre, rating, esrb, developer, publisher, mode, imgSrc, synopsis } = game
 
+  $img.setAttribute('class', 'game')
   $img.setAttribute('src', imgSrc)
 
   $detailOne.textContent = 'Genre: ' + genre
@@ -53,3 +113,4 @@ function renderGame(game) {
   $game.appendChild($detailList)
   return $game
 }
+*/
