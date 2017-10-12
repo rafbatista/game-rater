f5811aa2d829876315903f8e0b6d46ff367fa1df
rafajr91 <rah5024@gmail.com>
2017-10-12 10:17:10 -0700

    Defined GET request that pulled list of video games from database. #1

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
 const app = express()
 const bodyParser = require('body-parser')
+
+MongoClient.connect('mongodb://localhost/game-rater', (err, db) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+
+  const games = db.collection('games')
+
+  app.get('/games/all', (req, res) => {
+    games
+      .find({})
+      .toArray()
+      .then(games => res.json(games))
+      .catch(err => {
+        console.error(err)
+        res.sendStatus(500)
+      })
+  })
+  app.listen(3000, () => console.log('Test at http://localhost:3000'))
+})
