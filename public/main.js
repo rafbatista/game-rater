05415fe99d405f2132f438fa49e60300c19f2167
rafajr91 <rah5024@gmail.com>
2017-10-12 14:16:02 -0700

    Created public folder for my static files to be hosted on my server. #1

---
+++ public/main.js
@@ -0,0 +1,55 @@
+const $gamesList = document.querySelector('.games-list')
+
+function getGames() {
+  fetch('http://localhost:3000/games/all')
+    .then(res => {
+      return res.json()
+    })
+    .then(games => {
+      games
+        .map(renderGame)
+        .forEach($game => {
+          $gamesList.appendChild($game)
+        })
+    })
+}
+
+getGames()
+
+function renderGame(game) {
+  const $game = document.createElement('div')
+  const $gameTitle = document.createElement('h3')
+  const $img = document.createElement('img')
+  const $detailList = document.createElement('ul')
+  const $detailOne = document.createElement('li')
+  const $detailTwo = document.createElement('li')
+  const $detailThree = document.createElement('li')
+  const $detailFour = document.createElement('li')
+  const $detailFive = document.createElement('li')
+  const $detailSix = document.createElement('li')
+  const $detailSeven = document.createElement('li')
+  const { genre, rating, esrb, developer, publisher, mode, imgSrc, synopsis } = game
+
+  $img.setAttribute('src', imgSrc)
+
+  $detailOne.textContent = 'Genre: ' + genre
+  $detailTwo.textContent = 'Rating: ' + rating
+  $detailThree.textContent = 'ESRB: ' + esrb
+  $detailFour.textContent = 'Developer: ' + developer
+  $detailFive.textContent = 'Publisher: ' + publisher
+  $detailSix.textContent = 'Mode: ' + mode
+  $detailSeven.textContent = 'Synopsis: ' + synopsis
+
+  $detailList.appendChild($detailOne)
+  $detailList.appendChild($detailTwo)
+  $detailList.appendChild($detailThree)
+  $detailList.appendChild($detailFour)
+  $detailList.appendChild($detailFive)
+  $detailList.appendChild($detailSix)
+  $detailList.appendChild($detailSeven)
+
+  $game.appendChild($gameTitle)
+  $game.appendChild($img)
+  $game.appendChild($detailList)
+  return $game
+}
