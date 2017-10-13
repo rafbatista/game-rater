f05126986ee1ed24aa340683a9c1250d0d98a555
rafajr91 <rah5024@gmail.com>
2017-10-13 13:50:24 -0700

    Added rating stars into my renderGameThumb func on main.js as well as made styling changes. Corrected genre for two items in seed.js. #2

--- public/main.js
+++ public/main.js
@@ -20,7 +20,9 @@ function renderGameThumb(game) {
   const $gameThumb = document.createElement('div')
   const $img = document.createElement('img')
   const $caption = document.createElement('div')
-  const $captionHeader = document.createElement('h3')
+  const $captionHeader = document.createElement('h5')
+  const $starContainer = document.createElement('div')
+  const $ratingStars = document.createElement('input')
   const $detailContainer = document.createElement('div')
   const $detailList = document.createElement('ul')
   const $detailOne = document.createElement('li')
@@ -30,17 +32,28 @@ function renderGameThumb(game) {
   const $rateButton = document.createElement('a')
   const $detailsButton = document.createElement('a')
 
-  const { name, genre, rating, esrb, imgSrc } = game
+  const { id, name, genre, rating, esrb, imgSrc } = game
 
-  $gameThumb.setAttribute('class', 'col-md-2 thumbnail')
+  $gameThumb.setAttribute('class', 'col-md-3 thumbnail')
 
   $img.setAttribute('class', 'game-icon')
   $img.setAttribute('src', imgSrc)
   $img.setAttribute('alt', name)
+  $img.style.width = '300px'
+  $img.style.height = '250px'
 
-  $caption.setAttribute('class', 'caption')
+  $caption.setAttribute('class', 'game-caption')
 
   $captionHeader.textContent = name
+
+  $ratingStars.setAttribute('id', 'input-' + id)
+  $ratingStars.setAttribute('name', 'input-' + id)
+  $ratingStars.setAttribute('class', 'rating rating-loading')
+  $ratingStars.setAttribute('data-min', '0')
+  $ratingStars.setAttribute('data-max', '5')
+  $ratingStars.setAttribute('data-step', '1')
+  $ratingStars.setAttribute('data-size', 'sm')
+  $starContainer.appendChild($ratingStars)
 
   $detailOne.textContent = 'Genre: ' + genre
   $detailTwo.textContent = 'Rating: ' + rating
@@ -49,7 +62,6 @@ function renderGameThumb(game) {
   $detailList.appendChild($detailOne)
   $detailList.appendChild($detailTwo)
   $detailList.appendChild($detailThree)
-
   $detailContainer.appendChild($detailList)
 
   $rateButton.setAttribute('href', '#')
@@ -65,6 +77,7 @@ function renderGameThumb(game) {
   $buttonContainer.appendChild($rateButton)
   $buttonContainer.appendChild($detailsButton)
 
+  $caption.appendChild($starContainer)
   $caption.appendChild($captionHeader)
   $caption.appendChild($detailContainer)
   $caption.appendChild($buttonContainer)
