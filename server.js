ee2fe3eb1c9abd495453991ce9f1ef4197e88dbe
rafajr91 <rah5024@gmail.com>
2017-10-19 15:55:34 -0700

    Created GET handler in server.js that pulls from the games database and filter suggestions based on games the user rated in order to see suggested games. #3

--- server.js
+++ server.js
@@ -38,6 +38,70 @@ MongoClient.connect('mongodb://localhost/game-rater', (err, db) => {
         res.sendStatus(500)
       })
   })
+
+  app.get('/games/suggested', (req, res) => {
+    const gameSuggestions = []
+    games
+      .find({})
+      .toArray()
+      .then(games => {
+        ratedGames
+          .find()
+          .toArray()
+          .then(ratedGames => {
+            for (let i = 0; i < games.length; i++) {
+              for (let j = 0; j < ratedGames.length; j++) {
+                if (games[i].id === ratedGames[j].id) {
+                  games[i].rating = ratedGames[j].rating
+                }
+              }
+              if (games[i].rating > 0) {
+                games.splice(i, 1)
+              }
+              if (gameSuggestions.length > 3) {
+                return
+              }
+              const stats = getGameStats(ratedGames)
+              if (stats.avgRatings.action >= 3 && games[i].genre === 'action' && stats.ratingCount.action === 2) {
+                gameSuggestions.push(games[i])
+                games.splice(i, 1)
+              }
+              if (stats.avgRatings.fighting >= 3 && games[i].genre === 'fighting' && stats.ratingCount.fighting === 2) {
+                gameSuggestions.push(games[i])
+                games.splice(i, 1)
+              }
+              if (stats.avgRatings.shooter >= 3 && games[i].genre === 'shooter' && stats.ratingCount.shooter === 2) {
+                gameSuggestions.push(games[i])
+                games.splice(i, 1)
+              }
+              if (stats.avgRatings.action < 3 && games[i].genre === 'action' && stats.ratingCount.action === 2) {
+                games.splice(i, 1)
+              }
+              if (stats.avgRatings.fighting < 3 && games[i].genre === 'fighting' && stats.ratingCount.fighting === 2) {
+                games.splice(i, 1)
+              }
+              if (stats.avgRatings.shooter < 3 && games[i].genre === 'shooter' && stats.ratingCount.shooter === 2) {
+                games.splice(i, 1)
+              }
+            }
+            function getRandomIntInclusive(min, max) {
+              min = Math.ceil(min)
+              max = Math.floor(max)
+              return Math.floor(Math.random() * (max - min + 1)) + min
+            }
+            if (gameSuggestions.length < 3) {
+              gameSuggestions.push(games[getRandomIntInclusive(0, games.length)])
+            }
+            res.json(gameSuggestions)
+            gameSuggestions.splice(0)
+          })
+      })
+      .catch(err => {
+        console.error(err)
+        res.sendStatus(500)
+      })
+  })
+
   app.use(bodyParser.json())
 
   const ratedGames = db.collection('rated-games')
@@ -55,3 +119,33 @@ MongoClient.connect('mongodb://localhost/game-rater', (err, db) => {
   })
   app.listen(3000, () => console.log('Test at http://localhost:3000'))
 })
+
+function getAverageRating(ratings, genre) {
+  const games = ratings.filter(game => game.genre === genre)
+  const sum = games.reduce((sum, game) => {
+    return sum + game.rating
+  }, 0)
+  return games.length ? sum / games.length : 0
+}
+
+function getGenreCount(ratings, genre) {
+  const genreCount = {}
+  const games = ratings.filter(game => game.genre === genre)
+  genreCount[genre] = games.length
+  return genreCount
+}
+
+function getGameStats(ratedGames) {
+  return {
+    avgRatings: {
+      action: getAverageRating(ratedGames, 'action'),
+      fighting: getAverageRating(ratedGames, 'fighting'),
+      shooter: getAverageRating(ratedGames, 'shooter')
+    },
+    ratingCount: {
+      action: getGenreCount(ratedGames, 'action'),
+      fighting: getGenreCount(ratedGames, 'fighting'),
+      shooter: getGenreCount(ratedGames, 'shooter')
+    }
+  }
+}
